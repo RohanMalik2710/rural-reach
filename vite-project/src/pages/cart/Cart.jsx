55e0529a3b2b722d0693895bfc8ade4d2a24d97d
SAHIL-KUMAR-EE <121251129+SAHIL-KUMAR-EE@users.noreply.github.com>
2024-04-13 02:51:24 +0530

    Update Cart.jsx

--- vite-project/src/pages/cart/Cart.jsx
+++ vite-project/src/pages/cart/Cart.jsx
@@ -3,7 +3,7 @@ import myContext from '../../context/data/myContext';
 import Layout from '../../components/layout/Layout';
 import Modal from '../../components/modal/Modal';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteFromCart } from '../../redux/cartSlice';
+import { addToCart, deleteFromCart } from '../../redux/cartSlice';
 import { toast } from 'react-toastify';
 
 function Cart() {
@@ -18,27 +18,31 @@ function Cart() {
   useEffect(() => {
     let temp = 0;
     cartItems.forEach((cartItem) => {
-      if (cartItem && cartItem.price && cartItem.description && cartItem.imageUrl) {
-        temp += parseInt(cartItem.price);
-      }
+        if (cartItem && cartItem.price && cartItem.description && cartItem.imageUrl) {
+            temp += parseInt(cartItem.price) * cartItem.quantity; // Multiply price by quantity
+        }
     });
     setTotalAmount(temp);
   }, [cartItems]);
 
-  // const shipping = parseInt(100);
   var shipping = parseInt(0);
-  // const grandTotal = 0;
 
-  if((totalAmount > 0)&&(totalAmount <= 300)){
-    shipping = 100;
+  if ((totalAmount > 0) && (totalAmount <= 300)) {
+      shipping = 100;
   }
   var grandTotal = shipping + totalAmount;
+
 
   const deleteCart = (item) => {
     console.log("Delete Attempted",item);
     dispatch(deleteFromCart(item))
     toast.success('Item Deleted');
   }
+
+  const addCart = (product) => {
+    dispatch(addToCart(product))
+    toast.success('Added to Cart');
+}
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cartItems));
@@ -61,10 +65,20 @@ function Cart() {
                         <h2 className="text-sm text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>{item.description}</h2>
                         <p className="mt-1 text-xs font-semibold text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{item.price}</p>
                       </div>
-                      <div onClick={() => deleteCart(item)} className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                        </svg>
+                      <div className='flex ml-6 flex-col space-between'>
+                        <div className='ml-2 mb-10'>
+                          {item.quantity}
+                        </div>
+
+                        <div onClick={() => addCart(item)} className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6 mb-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
+                        </div>
+
+                        <div onClick={() => deleteCart(item)} className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24"><path d="M0 10h24v4h-24z"/></svg>
+                        </div>
+                        
+                        
                       </div>
                     </div>
                   </div>
