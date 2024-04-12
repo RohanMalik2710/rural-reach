9fba0c1699cc4cef605861168ccdd7220d903f53
SAHIL-KUMAR-EE <121251129+SAHIL-KUMAR-EE@users.noreply.github.com>
2024-04-13 02:50:47 +0530

    Update ProductCard.jsx

--- vite-project/src/components/productCard/ProductCard.jsx
+++ vite-project/src/components/productCard/ProductCard.jsx
@@ -11,7 +11,7 @@ function ProductCard() {
     
     const dispatch = useDispatch()
     const cartItems = useSelector((state) => state.cart)
-    // console.log("Currently",cartItems)
+    console.log("Currently",cartItems)
 
     // add to cart
     const addCart = (product) => {
